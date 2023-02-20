8048eb87702eb35c2fd8e4f577c777fe63a3f40b
Nhut Phan <thanquan@gmail.com>
2023-02-20 18:24:53 +0700

    feat: Set runtime = false for rustler

--- mix.exs
+++ mix.exs
@@ -21,9 +21,7 @@ defmodule ExWeb3.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.27.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler, "~> 0.27.0", runtime: false}
     ]
   end
 end
