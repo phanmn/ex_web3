a602a81961a480ac3409e0ce7daf39084dbea993
Nhut Phan <thanquan@gmail.com>
2023-02-20 18:56:36 +0700

    update: Feat add spec to ExWeb3.Wallet.validate_message?

--- lib/ex_web3/wallet.ex
+++ lib/ex_web3/wallet.ex
@@ -1,3 +1,4 @@
 defmodule ExWeb3.Wallet do
+  @spec validate_message?(bitstring(), bitstring(), bitstring()) :: boolean()
   defdelegate validate_message?(account, message, signature), to: ExWeb3.NativeWallet, as: :validate_message
 end
